f3caee03ab9aa92b2728356e5da1fdb05cd6534f
wpt <wei_pengtao@qq.com>
2025-09-19 21:28:54 +0800

    解决不能转发问题

--- xuecheng-plus-gateway/src/main/java/com/xuecheng/gateway/GatewayApplication.java
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/gateway/GatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
-        // TODO: 未实现用服务名称访问其他服务
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
