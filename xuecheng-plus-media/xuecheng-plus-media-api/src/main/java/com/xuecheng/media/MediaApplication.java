26c9c5423b00cd70d2d56e52364359f1707e7ff9
weipengtao <wei_pengtao@qq.com>
2025-09-25 13:17:08 +0800

    feature: 集成xxl-job

--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/MediaApplication.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/MediaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediaApiApplication {
+public class MediaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MediaApiApplication.class, args);
+        SpringApplication.run(MediaApplication.class, args);
     }
 
 }
